001e43e3b1a960ad9da8cc9f689042a21b9f25c6
Khs-NA <156624735+Khs-NA@users.noreply.github.com>
2024-01-16 21:48:54 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,38 +1,23 @@
-def find_nearest_sum(nan_list, target):
-    closest_sum = 0
-    selected_numbers = []
-    for i in range(len(nan_list)):
-        for j in range(i+1, len(nan_list)):
-            for k in range(j+1, len(nan_list)):
-                for l in range(k+1, len(nan_list)):
-                    for m in range(l+1, len(nan_list)):
-                        for n in range(m+1, len(nan_list)):
-                            for o in range(n+1, len(nan_list)):
-                                current_sum = nan_list[i] + nan_list[j] + nan_list[k] + nan_list[l] + nan_list[m] + nan_list[n] + nan_list[o]
-                                
-                                if current_sum <= target and current_sum > closest_sum:
-                                    closest_sum = current_sum
-                                    selected_numbers = [nan_list[i], nan_list[j], nan_list[k], nan_list[l], nan_list[m], nan_list[n], nan_list[o]]
-    return selected_numbers
-
-# 예시
-target = 100
-nan_list = [int(input()) for _ in range(9)]
+# find_dwarfs 함수는 9명의 난쟁이 중 2명을 선택하고,
+# 선택된 두 명을 제외한 나머지 7명의 키를 selected_heights 에 저장.
+# selected_heights 의 합이 100 이되면 리스트를 정렬하고 반환.
 
 
-result = find_nearest_sum(nan_list, target)
+def find_dwarfs(heights):
+    for i in range(9):
+        for j in range(i + 1, 9):
+            selected_heights = [heights[k] for k in range(9) if k != i and k != j]
+
+            if sum(selected_heights) == 100:
+                return sorted(selected_heights)
 
 
-# 오름차순 정렬
-n = len(result)
 
-for i in range(0, n-1):
-    least = i
-    for j in range(i+1, n):
-        if result[least] > result[j]:
-            least = j
-    result[i], result[least] = result[least], result[i]
 
-# 한줄씩 출력
-for number in result:
-    print(number)
+# 난쟁이들의 키 입력
+heights = [int(input()) for _ in range(9)]
+
+# 일곱 난쟁이를 찾아 정렬된 키를 출력
+result = find_dwarfs(heights)
+for height in result:
+    print(height)
