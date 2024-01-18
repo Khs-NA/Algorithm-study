94112c0a56c14e7bb1cdf0511f88766d7a5ba85e
Khs-NA <156624735+Khs-NA@users.noreply.github.com>
2024-01-18 23:57:21 +0900

    [Silver IV] Title: 색종이 - 2, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
@@ -1,7 +1,7 @@
 n = int(input())  # 색종이의 개수
 total_len = 0
 
-paper = [[0] * 101 for _ in range(101)]  # 100x100 크기의 종이를 만듭니다.
+paper = [[0] * 101 for _ in range(101)]  # 101x101 크기의 종이를 만듭니다.
 
 # 색종이를 붙이는 부분을 1로 마킹합니다.
 for _ in range(n):
@@ -10,15 +10,14 @@ for _ in range(n):
         for j in range(y, y + 10):
             paper[i][j] = 1
 
-# 색종이 1칸 주위에 0이 존재한다면 그건 변이라는 소리니깐 total_len에 1추가
-for i in range(100):
-    for j in range(100):
+# 각 좌표에 대해 상하좌우를 확인하여 색종이가 닿는 부분을 더합니다.
+for i in range(101):
+    for j in range(101):
         if paper[i][j] == 1:
-            # 색종이 1칸 주위에 0이 1개 존재한다면 그건 변이라는 소리니깐 total_len에 1추가
-            if (i > 0 and paper[i-1][j] == 0) or (i < 100 and paper[i+1][j] == 0) or (j < 100 and paper[i][j+1] == 0) or (j > 0 and paper[i][j-1] == 0):
-                total_len += 1
-            # 색종이 1칸 주위에 0이 2개 존재한다면 그건 모서리라는 소리니깐 total_len에 하나 더 1추가
-            if ((i > 0 and paper[i-1][j] == 0) and (j < 100 and paper[i][j+1] == 0)) or ((i > 0 and paper[i-1][j] == 0) and (j > 0 and paper[i][j-1] == 0)) or ((i < 100 and paper[i+1][j] == 0) and (j < 100 and paper[i][j+1] == 0)) or ((i < 100 and paper[i+1][j] == 0) and (j > 0 and paper[i][j-1] == 0)):
-                total_len += 1
+            total_len += 4
+            if i > 0 and paper[i - 1][j] == 1:
+                total_len -= 2
+            if j > 0 and paper[i][j - 1] == 1:
+                total_len -= 2
 
 print(total_len)
