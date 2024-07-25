f3270aa41c9c3f5ee5b05eb948ceff1b4e8c20e6
Khs-NA <156624735+Khs-NA@users.noreply.github.com>
2024-07-25 23:52:51 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,16 @@
+N = int(input())
+stair = [0]
+for _ in range(N):
+    stair.append(int(input()))
+
+# dp 배열 초기화
+dp = [0] * (N + 1)
+dp[1] = stair[1]
+
+if N > 1:
+    dp[2] = stair[1] + stair[2]
+
+for i in range(3, N + 1):
+    dp[i] = max(dp[i-2] + stair[i], dp[i-3] + stair[i-1] + stair[i])
+
+print(dp[N])
