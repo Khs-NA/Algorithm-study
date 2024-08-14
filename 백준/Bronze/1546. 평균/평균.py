ad95efc16041c9af289512b9fd445e76d0334d42
Khs-NA <156624735+Khs-NA@users.noreply.github.com>
2024-08-14 21:53:21 +0900

    [Bronze I] Title: 평균, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.py
@@ -0,0 +1,10 @@
+N = int(input())
+scores = list(map(int,input().split()))
+max_score = max(scores)
+
+new_scores = []
+for score in scores:
+    new_scores.append(score/max_score*100)
+
+result = sum(new_scores) / N
+print(result)
