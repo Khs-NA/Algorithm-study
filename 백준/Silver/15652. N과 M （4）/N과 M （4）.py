4d69d7a7571c0f53fc484c88b82d888a6a1c57c4
Khs-NA <156624735+Khs-NA@users.noreply.github.com>
2024-08-26 09:11:12 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,13 @@
+def combinations(n, m, start=1, chosen=[]):
+    if len(chosen) == m:
+        print(' '.join(map(str, chosen)))
+        return
+
+    for i in range(start, n + 1):
+        combinations(n ,m, i, chosen + [i])
+
+
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    combinations(N, M)
